bec824ff336b4db09d31985430e76274f35b39cb
Shivansh Singh Rathore <rathoreshivansh54@gmail.com>
2025-12-20 21:41:46 +0530

    version 1.0.5

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './css/Footer.css'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons'
+const Footer = () => {
+    return (
+        <>
+            <section className="footer">
+                <div className="footerAbout">
+                    <h3>EasyServ</h3>
+                    <p>We provide trusted home services including electricians, plumbing, cleaning, and AC & appliance repair. <br />Our certified professionals ensure quick, reliable, and affordable solutions — all at your doorstep.  </p>
+                </div>
+                <div className="footerServices">
+                    <h5>Services</h5>
+                    <ul>
+                        <li><Link to={'/services/electricians'}>Electricians</Link></li>
+                        <li><Link to={'/services/plumbers'}>Plumbers</Link></li>
+                        <li><Link to={'/services/cleaning'}>Cleaning</Link></li>
+                        <li><Link to={'/services/painting'}>Painting</Link></li>
+                        <li><Link to={'/services/carpentry'}>Carpentry</Link></li>
+                        <li><Link to={'/services'}>More services</Link></li>
+                    </ul>
+                </div>
+                <div className="footerLinks footerServices">
+                    <h5>Quick Links</h5>
+                    <ul>
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'/about'}>About Us</Link></li>
+                        <li><Link to={'/services'}>Services</Link></li>
+                        <li><Link to={'/contact'}>Contact Us</Link></li>
+                        <li><Link to={'/signup'}>Become a Provider</Link></li>
+                        <li><Link to={'/privacypolicy'}>Privacy Policy</Link></li>
+                        <li><Link to={'/tnc'}>Terms & Conditions</Link></li>
+                    </ul>
+                </div>
+                <div className='footerContact footerServices'>
+                    <h5>Contact Us</h5>
+                    <ul>
+                        <li>📍 123 Main Street, Bhopal, MP</li>
+                        <li><Link to={'tel:+919876543210'}>📞 +91 98765 43210</Link></li>
+                        <li><Link to={'mailto:support@eazyserv.com'}>✉️ support@eazyserv.com</Link></li>
+                    </ul>
+                    <div className="footerSocial">
+                        <Link to={'https://facebook.com/eazyserv'}><FontAwesomeIcon className='socialIcon' icon={faFacebook} /></Link>
+                        <Link to={'https://instagram.com/eazyserv'}><FontAwesomeIcon className='socialIcon' icon={faInstagram} /></Link>
+                        <Link to={'https://x.com/eazyserv'}><FontAwesomeIcon className='socialIcon' icon={faXTwitter} /></Link>
+                        <Link to={'https://linkedin.com/company/eazyserv'}><FontAwesomeIcon className='socialIcon' icon={faLinkedin} /></Link>
+                    </div>
+                </div>
+            </section>
+            <section className='copyright'>
+                <p>&copy; 2025 EasyServ. All rights reserved. </p>
+            </section>
+        </>
+    )
+}
+
+export default Footer
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,97 +1,143 @@
 import React from 'react'
 import './css/Home.css'
 import Navbar from '../components/Navbar'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
 const Home = () => {
   const categories = [
-    { 
+    {
       "name": "Electrician",
       "desc": "Certified electricians for wiring, fittings, and quick electrical repairs at home."
     },
-    { 
+    {
       "name": "Plumber",
       "desc": "Expert plumbers for leak repairs, pipe fitting, and bathroom installations."
     },
-    { 
+    {
       "name": "Cleaning",
       "desc": "Professional home, kitchen, and deep cleaning services for a spotless living space."
     },
-    { 
+    {
       "name": "AC & Appliance Repair",
       "desc": "Reliable technicians to fix ACs, refrigerators, washing machines, and more."
     },
   ]
   return (
     <div className='home'>
-    <Navbar/>
-    <section className="homeHero">
-      <div className="leftHero">
-        <div className="heroHead">
-          <h3>Find Trusted Professionals for Every Home Service</h3>
-          <p>Verified Experts. Transparent Pricing. Quick Booking.</p>
-        </div>
-        <div className="heroBtns">
-          <button className="primaryBtn">Book a Service</button>
-          <button className="secondaryBtn">Become a Provider</button>
-        </div>
-        <div className="heroSearchBar">
-          <input type="search" placeholder='What Service do you need?' />
-        </div>
-      </div>
-      <div className="rightHero">
-        <img src="/static/images/hero-right.svg" alt="" />
-      </div>
-    </section>
-    <section className="serviceSection">
-      <h1>Popular Categories</h1>
-      <div className="categoriesGrid">
-        {categories.map((category,index)=>(
-        <div key={index} className="categoryBox">
-          <img src={`/static/images/services/${category.name}.jpg`} alt="" />
-          <div className="aboutCategory">
-            <h3>{category.name}</h3>
-            <p>{category.desc}</p>
+      <Navbar />
+      <section className="homeHero">
+        <div className="leftHero">
+          <div className="heroHead">
+            <h3>Find Trusted Professionals for Every Home Service</h3>
+            <p>Verified Experts. Transparent Pricing. Quick Booking.</p>
           </div>
-          <button className="primaryBtn">Book Now</button>
+          <div className="heroBtns">
+            <button className="primaryBtn">Book a Service</button>
+            <button className="secondaryBtn">Become a Provider</button>
+          </div>
+          <div className="heroSearchBar">
+            <input type="search" placeholder='What Service do you need?' />
+          </div>
         </div>
-        ))
-        }
-      </div>
-      <Link to={'/services'}>Explore All Services &rarr;</Link>
-    </section>
-    <section className="howItWorks">
-      <div className="hiwHead">
-        <h1>How It Works</h1>
-        <p>Getting things done in just 4 simple steps</p>
-      </div>
-      <div className="steps">
-        {[
-          {
-            "name":"Search",
-            "desc":"Find the service you need."
-          },
-          {
-            "name":"Book",
-            "desc":"Select date, time and confirm Booking."
-          },
-          {
-            "name":"Worker Arrives",
-            "desc":"Professional comes to your doorstep."
-          },
-          {
-            "name":"Pay",
-            "desc":"Hassle-free payment after service."
+        <div className="rightHero">
+          <img src="/static/images/hero-right.svg" alt="" />
+        </div>
+      </section>
+      <section className="serviceSection">
+        <h1>Popular Categories</h1>
+        <div className="categoriesGrid">
+          {categories.map((category, index) => (
+            <div key={index} className="categoryBox">
+              <img src={`/static/images/services/${category.name}.jpg`} alt="" />
+              <div className="aboutCategory">
+                <h3>{category.name}</h3>
+                <p>{category.desc}</p>
+              </div>
+              <button className="primaryBtn">Book Now</button>
+            </div>
+          ))
           }
-        ].map((step,index)=>(
-          <div key={index} className="step">
-            <h1>{index+1}</h1>
-            <h3>{step.name}</h3>
-            <p>{step.desc}</p>
+        </div>
+        <Link to={'/services'}>Explore All Services &rarr;</Link>
+      </section>
+      <section className="howItWorks">
+        <div className="hiwHead">
+          <h1>How It Works</h1>
+          <p>Getting things done in just 4 simple steps</p>
+        </div>
+        <div className="steps">
+          {[
+            {
+              "name": "Search",
+              "desc": "Find the service you need."
+            },
+            {
+              "name": "Book",
+              "desc": "Select date, time and confirm Booking."
+            },
+            {
+              "name": "Worker Arrives",
+              "desc": "Professional comes to your doorstep."
+            },
+            {
+              "name": "Pay",
+              "desc": "Hassle-free payment after service."
+            }
+          ].map((step, index) => (
+            <div key={index} className="step">
+              <h1>{index + 1}</h1>
+              <h3>{step.name}</h3>
+              <p>{step.desc}</p>
+            </div>
+          ))
+          }
+        </div>
+      </section>
+      <section className="whyChooseUs">
+        <h1>Why Choose Us?</h1>
+        <div className="features">
+          <div className="featuresLeft">
+            <div className="featureBox">
+              <h3>Verified Experts</h3>
+              <p>Every professional is background-checked and trained.</p>
+            </div>
+            <div className='featureBox'></div>
+            <div className="featureBox">
+              <h3>Affordable Pricing</h3>
+              <p>Transparent rates with no hidden charges.</p>
+            </div>
+            <div className='featureBox'></div>
           </div>
-        ))
-        }
-      </div>
-    </section>
+          <div className="centerLine"></div>
+          <div className="featureIcons">
+            <div className="box">
+              <div className="featureIcon">👷</div>
+            </div>
+            <div className="box">
+              <div className="featureIcon">⏰</div>
+            </div>
+            <div className="box">
+              <div className="featureIcon">💸</div>
+            </div>
+            <div className="box">
+              <div className="featureIcon">💬</div>
+            </div>
+          </div>
+          <div className="featuresRight">
+            <div className='featureBox'></div>
+            <div className="featureBox">
+              <h3>On-Time Service</h3>
+              <p>Get your service exactly when you need it.</p>
+            </div>
+            <div className='featureBox'></div>
+            <div className="featureBox">
+              <h3>24/7 Support</h3>
+              <p>Always available for queries and rescheduling.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer/>
     </div>
   )
 }
